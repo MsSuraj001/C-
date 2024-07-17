fcc4f8a69440e6980b4babfff7c36f6a5fd4948b
MsSuraj001 <milukumar225@gmail.com>
2024-07-17 22:22:44 +0530

    learn the some function problem in cpp

---
+++ Function/prectic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int n = 5;
+    int value =0;
+    for(int i=0; i>=4; i*=2){
+        value++;
+        cout<<i;
+    }
+            // cout<<i;
+
+}
