5815987c919a3789945428ec9dc019c5d4f805d8
MsSuraj001 <milukumar225@gmail.com>
2024-07-17 22:31:08 +0530

    learn the some problem in cpp

---
+++ Function/functionToFactorial.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void factorial(int x){
+    int fact = 1;
+    for(int i=0; i<n; i++){
+        fact = fact*i;
+    }
+    return;
+}
+int main(){
+    int n;
+    cout<<"Enter N: ";
+    cin>>n;
+    int r; 
+    cout<<"Enter R: ";
+    cin>>r;
+    int factorial(n);
+    int factorial(r);
+    int factorial (n-r);
+
+    int ncr = (fact(n))/(fact(r)*fact(n-r));
+    cout<<ncr;
+    return 0;
+}
