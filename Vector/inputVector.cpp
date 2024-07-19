19489cbc658641d3fc2bf6de0553a737a80bb19e
MsSuraj001 <milukumar225@gmail.com>
2024-07-19 22:25:35 +0530

    learn the vector in cpp

---
+++ Vector/inputVector.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v;
+    for(int i=0; i<5; i++){
+        int element;
+        cin>>element;
+        v.push_back(element);
+        }
+        //for(int i=0; i<5; i++){
+          //  cout<<v[i];
+        // }
+        for(int ele: v){
+            cout<<ele<<endl;
+        }
+
+    return 0;
+}
