d9bdc51f06de9eb884b4ca8feb0a7c9815add55f
MsSuraj001 <milukumar225@gmail.com>
2024-08-01 17:45:17 +0530

    learn the array in cpp

---
+++ Array/arrayPractic.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+   // int arr[] = {1,2,3,4,5};
+    // int arr[] = {1,4,3,2,6};
+    // cout<<sizeof(arr)<<endl;
+    // cout<<sizeof(arr)/sizeof(arr[0]);
+    // string arr[4] = {"bmw","volvo","marstiz","car"};
+    // for(int i=0; i<4; i++){
+    //     cout<<i <<"="<<arr[i]<<endl;
+    // }
+    vector<int>v(4);
+    v = {1,2,3,4};
+    
+
+    return 0;
+}
